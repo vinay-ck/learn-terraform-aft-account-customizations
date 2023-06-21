5f920743edd861e1525e39e484a487d80bdf1e4c
vinay-ck <vinay.naidu@cloud-kinetics.com>
2023-06-21 11:31:50 +0800

    add customization

--- enterprise-customizations/terraform/s3.tf
+++ enterprise-customizations/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-enterprise-customizations-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
